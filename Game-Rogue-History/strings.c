d0921696deeb0d0cfc2b5cfb7e44909a95a9f188
unknown <mikeyk730@gmail.com>
2016-10-08 03:58:19 -0400

    EpyxRogueDOS148Source

--- Game-Rogue-History/strings.c
+++ Game-Rogue-History/strings.c
@@ -1,87 +1,83 @@
-
-
 extern char ctp_[];
 
-isalpha(x) {	return  x > 128 ? 0 : (ctp_[(x)+1]&0x03); }
-isupper(x) {	return  x > 128 ? 0 : (ctp_[(x)+1]&0x01); }
-islower(x) {	return  x > 128 ? 0 : (ctp_[(x)+1]&0x02); }
-isdigit(x) {	return  x > 128 ? 0 : (ctp_[(x)+1]&0x04); }
-isspace(x) {	return  x > 128 ? 0 : (ctp_[(x)+1]&0x10); }
-isprint(x) {	return  x > 128 ? 0 : (ctp_[(x)+1]&0xc7); }
-
-toascii(x)
+isalpha(int x)
 {
-	return (x&127);
+  return x>128?0:(ctp_[(x)+1]&0x03);
 }
 
-toupper(chr)
-	char chr;
+isupper(int x)
 {
-    return(islower(chr)?((chr)-('a'-'A')):(chr));
+  return x>128?0:(ctp_[(x)+1]&0x01);
 }
 
-tolower(chr)
-	char chr;
+islower(int x)
 {
-	return(isupper(chr)?((chr)+('a'-'A')):(chr));
+  return x>128?0:(ctp_[(x)+1]&0x02);
 }
 
-stccpy(s1,s2,count)
-	char *s1, *s2;
-	int count;
+isdigit(int x)
 {
-	while (count-->0 && *s2)
-		*s1++ = *s2++;
-	*s1 = 0;
-	/*
-	 * lets return the address of the end of the string so
-	 * we can use that info if we are going to cat on something else!!
-	 */
-	return (s1);
+  return x>128?0:(ctp_[(x)+1]&0x04);
 }
 
-
-/*
- * redo Lattice token parsing routines
- */
-char *
-stpblk(str)
-	char *str;
+isspace(int x)
 {
-	while (isspace(*str))
-		str++;
-	return(str);
+  return x>128?0:(ctp_[(x)+1]&0x10);
 }
 
-stpbrk(str,brk)
-	char *str,*brk;
+isprint(int x)
 {
-	while(*str && !index(brk,*str))
-		str++;
-	return(*str ? str : 0);
+  return x>128?0:(ctp_[(x)+1]&0xc7);
 }
 
-
-/*
- * remove trailing whitespace from the end of a line
- */
-endblk(str)
-	char *str;
+toascii(int x)
 {
-    register char *backup;
-
-    backup = str + strlen(str);
-    while (backup != str && isspace(*(--backup)))
-    	*backup = 0;
-	return(str);
+  return (x&127);
 }
 
-/*
- * lcase: convert a string to lower case
- */
-lcase(str)
-	char *str;
+toupper(char chr)
 {
-	while ( *str = tolower(*str) )
-		str++;
+  return (islower(chr)?((chr)-('a'-'A')):(chr));
+}
+
+tolower(char chr)
+{
+  return (isupper(chr)?((chr)+('a'-'A')):(chr));
+}
+
+stccpy(char *s1, char *s2, int count)
+{
+  while (count-->0 && *s2) *s1++ = *s2++;
+  *s1 = 0;
+  //let's return the address of the end of the string so we can use that info if we are going to cat on something else!!
+  return (s1);
+}
+
+//redo Lattice token parsing routines
+char *stpblk(char *str)
+{
+  while (isspace(*str)) str++;
+  return (str);
+}
+
+stpbrk(char *str, char *brk)
+{
+  while (*str && !index(brk,*str)) str++;
+  return (*str?str:0);
+}
+
+//remove trailing whitespace from the end of a line
+endblk(char *str)
+{
+  char *backup;
+
+  backup = str+strlen(str);
+  while (backup!=str && isspace(*(--backup))) *backup = 0;
+  return (str);
+}
+
+//lcase: convert a string to lower case
+lcase(char *str)
+{
+  while (*str = tolower(*str)) str++;
 }
